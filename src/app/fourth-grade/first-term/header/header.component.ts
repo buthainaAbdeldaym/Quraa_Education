2d32ab435d5de6f6d9923218d6fff21beba3eaa6
buthainaAbdeldaym <buthaina.abdeldaym1305@gmail.com>
2025-04-09 15:08:15 +0200

    making responsive design and update in header component

--- src/app/fourth-grade/first-term/header/header.component.ts
+++ src/app/fourth-grade/first-term/header/header.component.ts
@@ -27,12 +27,76 @@ export class HeaderComponent implements OnInit,OnDestroy{
           this.sections.splice(section,1);
         }
       }
-      if (this.currentUrl.includes('unite-one')&&this.currentUrl.includes('lesson-one')) {
+
+      if (this.currentUrl.includes('social-studies-content')) {
+        if(!this.sections.includes('الدراسات الاجتماعيه')){
+          this.sections.push('الدراسات الاجتماعيه');
+        }
+      }
+      else if (!this.currentUrl.includes('social-studies-content') && this.currentUrl.includes('first-term-subjects')) {
+        if(this.sections.includes('الدراسات الاجتماعيه')){
+          let section=this.sections.indexOf('الدراسات الاجتماعيه')
+          this.sections.splice(section,1);
+        }
+      }
+
+      if (this.currentUrl.includes('mathematics-content')) {
+        if(!this.sections.includes('الرياضيات')){
+          this.sections.push('الرياضيات');
+        }
+      }
+      else if (!this.currentUrl.includes('mathematics-content') && this.currentUrl.includes('first-term-subjects')) {
+        if(this.sections.includes('الرياضيات')){
+          let section=this.sections.indexOf('الرياضيات')
+          this.sections.splice(section,1);
+        }
+      }
+
+      if (this.currentUrl.includes('science-content')) {
+        if(!this.sections.includes('العلوم')){
+          this.sections.push('العلوم');
+        }
+      }
+      else if (!this.currentUrl.includes('science-content') && this.currentUrl.includes('first-term-subjects')) {
+        if(this.sections.includes('العلوم')){
+          let section=this.sections.indexOf('العلوم')
+          this.sections.splice(section,1);
+        }
+      }
+
+
+      if (this.currentUrl.includes('islamic-studies-content')) {
+        if(!this.sections.includes('التربية الدينية الاسلامية')){
+          this.sections.push('التربية الدينية الاسلامية');
+        }
+      }
+      else if (!this.currentUrl.includes('islamic-studies-content') && this.currentUrl.includes('first-term-subjects')) {
+        if(this.sections.includes('التربية الدينية الاسلامية')){
+          let section=this.sections.indexOf('التربية الدينية الاسلامية')
+          this.sections.splice(section,1);
+        }
+      }
+
+
+      if (this.currentUrl.includes('ict-content')) {
+        if(!this.sections.includes('الكمبيوتر وتكنولوجيا المعلومات')){
+          this.sections.push('الكمبيوتر وتكنولوجيا المعلومات');
+        }
+      }
+      else if (!this.currentUrl.includes('ict-content') && this.currentUrl.includes('first-term-subjects')) {
+        if(this.sections.includes('الكمبيوتر وتكنولوجيا المعلومات')){
+          let section=this.sections.indexOf('الكمبيوتر وتكنولوجيا المعلومات')
+          this.sections.splice(section,1);
+        }
+      }
+
+
+      if (this.currentUrl.includes('unit-one')&&this.currentUrl.includes('lesson-one')) {
         if (!this.sections.includes('الدرس الاول فى الوحده الاولى')) {
           this.sections.push('الدرس الاول فى الوحده الاولى');
         }
       }
-      else if(this.currentUrl.includes('unite-one')&&!this.currentUrl.includes('lesson-one')){
+      else if(this.currentUrl.includes('unit-one')&&!this.currentUrl.includes('lesson-one')){
 
         if (this.sections.includes('الدرس الاول فى الوحده الاولى')) {
           let section=this.sections.indexOf('الدرس الاول فى الوحده الاولى')
@@ -40,6 +104,10 @@ export class HeaderComponent implements OnInit,OnDestroy{
         }
       }
     }
+
+
+
+
 
     this.subscription = this.__Router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
@@ -55,12 +123,74 @@ export class HeaderComponent implements OnInit,OnDestroy{
             this.sections.splice(section,1);
           }
         }
-        if (this.currentUrl.includes('unite-one')&&this.currentUrl.includes('lesson-one')) {
+
+        if (this.currentUrl.includes('social-studies-content')) {
+          if(!this.sections.includes('الدراسات الاجتماعيه')){
+            this.sections.push('الدراسات الاجتماعيه');
+          }
+        }
+        else if (!this.currentUrl.includes('social-studies-content') && this.currentUrl.includes('first-term-subjects')) {
+          if(this.sections.includes('الدراسات الاجتماعيه')){
+            let section=this.sections.indexOf('الدراسات الاجتماعيه')
+            this.sections.splice(section,1);
+          }
+        }
+
+        if (this.currentUrl.includes('mathematics-content')) {
+          if(!this.sections.includes('الرياضيات')){
+            this.sections.push('الرياضيات');
+          }
+        }
+        else if (!this.currentUrl.includes('mathematics-content') && this.currentUrl.includes('first-term-subjects')) {
+          if(this.sections.includes('الرياضيات')){
+            let section=this.sections.indexOf('الرياضيات')
+            this.sections.splice(section,1);
+          }
+        }
+
+        if (this.currentUrl.includes('science-content')) {
+          if(!this.sections.includes('العلوم')){
+            this.sections.push('العلوم');
+          }
+        }
+        else if (!this.currentUrl.includes('science-content') && this.currentUrl.includes('first-term-subjects')) {
+          if(this.sections.includes('العلوم')){
+            let section=this.sections.indexOf('العلوم')
+            this.sections.splice(section,1);
+          }
+        }
+
+        if (this.currentUrl.includes('islamic-studies-content')) {
+          if(!this.sections.includes('التربية الدينية الاسلامية')){
+            this.sections.push('التربية الدينية الاسلامية');
+          }
+        }
+        else if (!this.currentUrl.includes('islamic-studies-content') && this.currentUrl.includes('first-term-subjects')) {
+          if(this.sections.includes('التربية الدينية الاسلامية')){
+            let section=this.sections.indexOf('التربية الدينية الاسلامية')
+            this.sections.splice(section,1);
+          }
+        }
+
+        if (this.currentUrl.includes('ict-content')) {
+          if(!this.sections.includes('الكمبيوتر وتكنولوجيا المعلومات')){
+            this.sections.push('الكمبيوتر وتكنولوجيا المعلومات');
+          }
+        }
+        else if (!this.currentUrl.includes('ict-content') && this.currentUrl.includes('first-term-subjects')) {
+          if(this.sections.includes('الكمبيوتر وتكنولوجيا المعلومات')){
+            let section=this.sections.indexOf('الكمبيوتر وتكنولوجيا المعلومات')
+            this.sections.splice(section,1);
+          }
+        }
+
+
+        if (this.currentUrl.includes('unit-one')&&this.currentUrl.includes('lesson-one')) {
           if (!this.sections.includes('الدرس الاول فى الوحده الاولى')) {
             this.sections.push('الدرس الاول فى الوحده الاولى');
           }
         }
-        else if(this.currentUrl.includes('unite-one')&&!this.currentUrl.includes('lesson-one')){
+        else if(this.currentUrl.includes('unit-one')&&!this.currentUrl.includes('lesson-one')){
 
           if (this.sections.includes('الدرس الاول فى الوحده الاولى')) {
             let section=this.sections.indexOf('الدرس الاول فى الوحده الاولى')
